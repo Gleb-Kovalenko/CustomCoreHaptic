f2b684bb5a7a45ef80e83457f5af3bfd1d6b067f
Глеб Коваленко <glebkovalenko@MacBook-Air-Gleb.local>
2023-01-24 15:57:17 +0300

    initial commit

---
+++ CustomCoreHaptic/CustomCoreHapticApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCoreHapticApp.swift
+//  CustomCoreHaptic
+//
+//  Created by Глеб Коваленко on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomCoreHapticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
